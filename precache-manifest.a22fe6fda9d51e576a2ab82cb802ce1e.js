1bb09a78ce55d2e6a8ff63fe8ab6b10b1dba8fb6
pwsamiam <pwsamiam@gmail.com>
2020-09-05 22:08:31 -0400

    Updates

---
+++ precache-manifest.a22fe6fda9d51e576a2ab82cb802ce1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf7711e86206878364372a416ef7bf7",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "a4245f201bd27eb2cb38",
+    "url": "/simple-todo-app/static/css/main.2c752cb2.chunk.css"
+  },
+  {
+    "revision": "4dd3a872029e2042b12a",
+    "url": "/simple-todo-app/static/js/2.db6b3004.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.db6b3004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4245f201bd27eb2cb38",
+    "url": "/simple-todo-app/static/js/main.9b7a5b9d.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
